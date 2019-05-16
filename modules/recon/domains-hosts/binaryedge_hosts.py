91799c429ccb021e9935f6f0f0c2e94b49502a3b
Ryan Hays <hays.ryan@gmail.com>
2019-05-16 06:15:34 -0800

    Added new Binary Edge module to query hosts by domain

---
+++ modules/recon/domains-hosts/binaryedge_hosts.py
@@ -0,0 +1,24 @@
+from recon.core.module import BaseModule
+
+class Module(BaseModule):
+
+    meta = {
+        'name': 'BinaryEdge.io DNS lookup',
+        'author': 'Ryan Hays',
+        'version': '1.0',
+        'description': 'Uses the BinaryEdge API to discover subdomains.',
+        'required_keys': ['binaryedge_api'],
+        'query': 'SELECT DISTINCT domain FROM domains WHERE domain IS NOT NULL',
+    }
+
+    def module_run(self, domains):
+        key = self.get_key('binaryedge_api')
+        for domain in domains:
+            self.heading(domain, level=0)
+            resp = self.request(url=f"https://api.binaryedge.io/v2/query/domains/dns/{domain}", headers={'X-Key': key})
+            if resp.status_code == 200:
+                for subdomain in resp.json().get('events'):
+                    try:
+                        self.insert_hosts(host=subdomain['domain'], ip_address=subdomain['A'][0])
+                    except KeyError:
+                        self.insert_hosts(host=subdomain['domain'])
