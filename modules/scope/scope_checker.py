c5a252b9392a1c94f0789fe17765c82d57e2287b
Ryan Hays <hays.ryan@gmail.com>
2019-06-03 21:22:40 -0400

    Added functionality for checking domains within the hosts table as well as IPs and CIDR.

--- modules/scope/scope_checker.py
+++ modules/scope/scope_checker.py
@@ -1,5 +1,7 @@
 from recon.core.module import BaseModule
 import ipaddress
+import os
+import socket
 
 
 class Module(BaseModule):
@@ -7,22 +9,43 @@ class Module(BaseModule):
         'name': 'Scope Checker',
         'author': 'Ryan Hays',
         'version': '1.0',
-        'description': 'Compares the hosts table and removes hosts that are not found within the provided scope document. '
-                       'Scope should be defined within the document either by single IP or in CIDR notation (eg. 192.168.1.0/24)',
+        'description': 'Compares the hosts table and removes hosts that are not found within the provided scope '
+                       'document. Scope should be defined within the document by one entry per line either by single '
+                       'domain, IP, or CIDR notation (eg. google.com, 192.168.1.1, or 192.168.1.0/24)',
         'query': 'SELECT DISTINCT ip_address FROM hosts WHERE ip_address IS NOT NULL',
         'options': (
             ('filename', None, True, 'path and filename for scope document'),
         ),
+        'files': (
+            'tld_list.txt',
+        ),
     }
 
     def module_run(self, hosts):
+        tld_list = []
+        with open(os.path.join(self.data_path, 'tld_list.txt'), 'r') as fp:
+            for tld in fp:
+                if '#' != tld[0]:
+                    tld_list.append(tld.rstrip().lower())
+
         cidr_dict = {}
         with open(self.options['filename'], 'rt') as fp:
             for cidr in fp:
+                cidr = cidr.rstrip()
+
                 try:
-                    cidr_dict[ipaddress.ip_network(cidr.rstrip())] = cidr.rstrip()
+                    cidr_dict[ipaddress.ip_network(cidr)] = cidr
                 except ValueError:
-                    self.error(f"Invalid CIDR notation for: {cidr.rstrip()}")
+                    if cidr.split('.')[-1].lower() in tld_list:
+                        try:
+                            ips = socket.gethostbyname_ex(cidr)[2]
+                            for ip in ips:
+                                cidr_dict[ipaddress.ip_network(ip)] = cidr
+                        except socket.gaierror:
+                            self.error(f"Couldn't lookup IP Address for: {cidr}")
+
+                    else:
+                        self.error(f"Invalid scope item: {cidr}")
 
         cnt = 0
         for host in hosts:
