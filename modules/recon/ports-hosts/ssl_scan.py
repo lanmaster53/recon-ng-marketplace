f6a891203990271b943c5041f20997b80343184f
Ryan Hays <hays.ryan@gmail.com>
2019-06-04 14:16:54 -0400

    Made some changes.

--- modules/recon/ports-hosts/ssl_scan.py
+++ modules/recon/ports-hosts/ssl_scan.py
@@ -9,21 +9,27 @@ class Module(BaseModule):
         'name': 'SSL Scanner SAN Lookup',
         'author': 'Ryan Hays (@_ryanhays)',
         'version': '1.0',
-        'description': 'Connects to a Host and pulls the Subject Alternative Names from the IP Address and Port combination.',
+        'description': 'Queries the ports table to build a list of IP Address:Ports. It then connects to each service '
+                       'updating the Ports table with the certificate common name and then adds the Subject Alt Names '
+                       'to the hosts table.',
         'query': 'SELECT DISTINCT ("ip_address" || ":" || "port") FROM ports WHERE ip_address IS NOT NULL',
     }
 
     def module_run(self, hosts):
         for host in hosts:
-            url, port = host.split(':')
+            ip, port = host.split(':')
             try:
-                cert = ssl.get_server_certificate((url, port))
+                cert = ssl.get_server_certificate((ip, port))
             except ssl.SSLError:
-                self.alert(f"This is not a proper HTTPS service: {host}:{port}")
+                self.alert(f"This is not a proper HTTPS service: {ip}:{port}")
                 continue
 
             x509 = M2Crypto.X509.load_cert_string(cert)
-            x509.get_ext('subjectAltName').get_value()
+            commonname = x509.get_subject().as_text().split('=')[-1]
+
+            self.output(f"Updating ports table for {ip} to include host {commonname}")
+            self.query('UPDATE ports SET ip_address=?, host=?, port=?, protocol=? WHERE ip_address=?',
+                       (ip, commonname, port, 'tcp', ip))
 
             for san in x509.get_ext('subjectAltName').get_value().split(','):
-                self.insert_ports(ip_address=url, port=port, host=san.split(':')[1], protocol='tcp')
+                self.insert_hosts(host=san.split(':')[1])
