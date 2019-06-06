4f35d627ee5649628e22b6f7a1cccc521d90ac17
Ryan Hays <hays.ryan@gmail.com>
2019-06-06 14:50:21 -0400

    Recoded this to utilize the namechk API. The previous implementation no longer worked.

--- modules/recon/profiles-profiles/namechk.py
+++ modules/recon/profiles-profiles/namechk.py
@@ -1,15 +1,16 @@
 from recon.core.module import BaseModule
 from recon.mixins.threads import ThreadingMixin
-from http.cookiejar import CookieJar
-from lxml.html import fromstring
+
 
 class Module(BaseModule, ThreadingMixin):
 
     meta = {
         'name': 'NameChk.com Username Validator',
-        'author': 'Tim Tomes (@LaNMaSteR53) and thrapt (thrapt@gmail.com)',
+        'author': 'Tim Tomes (@LaNMaSteR53) and thrapt (thrapt@gmail.com) and Ryan Hays (@_ryanhays)',
         'version': '1.0',
-        'description': 'Leverages NameChk.com to validate the existance of usernames on specific web sites and updates the \'profiles\' table with the results.',
+        'description': 'Leverages NameChk.com API to validate the existance of usernames on specific web sites and '
+                       'pdates the \'profiles\' table with the results.',
+        'required_keys': ['namechk_api'],
         'comments': (
             'Note: The global timeout option may need to be increased to support slower sites.',
         ),
@@ -17,54 +18,23 @@ class Module(BaseModule, ThreadingMixin):
     }
 
     def module_run(self, usernames):
-        # retrieve list of sites
-        self.verbose('Retrieving site data...')
-        url = 'https://namechk.com/'
-        cookiejar = CookieJar()
-        resp = self.request(url, cookiejar=cookiejar)
-        tree = fromstring(resp.text)
-        # extract sites info from the page
-        names = tree.xpath('//div[@class="media record"]/@data-name')
-        labels = tree.xpath('//div[@class="media record"]//h4[@class="media-heading"]/text()')
-        if not len(names) == len(labels):
-            self.error('Inconsistent number of sites and labels.')
-            return
-        # merge names and labels into a list of tuples
-        sites = zip(names, labels)
-        # extract token from the reponse
-        token = ''.join([x.value for x in resp.cookiejar if x.name=='token'])
-        # reset url for site requests
-        url = 'https://namechk.com/availability/%s'
-        payload = {'x': token}
-        # required header for site requests
-        headers = {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json'}
-        for username in usernames:
-            self.heading(username, level=0)
-            payload['q'] = username
-            # validate memberships
-            self.thread(sites, url, payload, headers, cookiejar)
+        key = self.get_key('namechk_api')
+        # Gets a list of available services
+        avail_sites = self.request(url=f"https://api.namechk.com/services/available.json",
+                                   headers={'AUTHORIZATION': f"Bearer {key}", 'Accept': 'application/vnd.api.v1+json'})
 
-    def module_thread(self, site, url, payload, headers, cookiejar):
-        name, label = site
-        fails = 1
-        retries = 5
-        while True:
-            # build and send the request
-            resp = self.request(url % name, headers=headers, payload=payload, cookiejar=cookiejar)
-            # retry a max # of times for server 500 error
-            if 'error' in resp.json():
-                if fails < retries:
-                    fails += 1
-                    continue
-                self.error(f"{label}: Unknown error!")
-            else:
-                username = resp.json()['username']
-                available = resp.json()['available']
-                #status = resp.json()['status']
-                #reason = resp.json()['failed_reason']
-                profile = resp.json()['callback_url']
-                if not available:
-                    # update profiles table
-                    self.insert_profiles(username=username, resource=label, url=profile, category='social')
-                    self.query('DELETE FROM profiles WHERE username = ? and url IS NULL', (username,))
-            break
+        if avail_sites.status_code == 200:
+            for username in usernames:
+                self.heading(username, level=0)
+                self.thread(avail_sites.json(), username, key)
+
+    def module_thread(self, site, username, key):
+        resp = self.request(url=f"https://api.namechk.com/services/check.json", method='POST',
+                            headers={'AUTHORIZATION': f"Bearer {key}", 'Accept': 'application/vnd.api.v1+json'},
+                            payload={'site': site['short_name'], 'username': username})
+
+        if resp.status_code == 200:
+            if not resp.json().get('available'):
+                self.insert_profiles(username=username, resource=site['name'], url=resp.json().get('callback_url'),
+                                     category=site['category'])
+                self.query('DELETE FROM profiles WHERE username = ? and url IS NULL', (username,))
