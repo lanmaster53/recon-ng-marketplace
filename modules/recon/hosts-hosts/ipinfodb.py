2ef86183329330c53934b715b5be60124656d001
Ryan Hays <hays.ryan@gmail.com>
2019-06-06 16:11:42 -0400

    Added the Options back in to accomodat

--- modules/recon/hosts-hosts/ipinfodb.py
+++ modules/recon/hosts-hosts/ipinfodb.py
@@ -8,13 +8,19 @@ class Module(BaseModule):
         'name': 'IPInfoDB GeoIP',
         'author': 'Tim Tomes (@LaNMaSteR53)',
         'version': '1.0',
-        'description': 'Leverages the ipinfodb.com API to geolocate a host by IP address. Updates the \'hosts\' table with the results.',
+        'description': 'Leverages the ipinfodb.com API to geolocate a host by IP address. Updates the \'hosts\' table '
+                       'with the results.',
         'required_keys': ['ipinfodb_api'],
         'query': 'SELECT DISTINCT ip_address FROM hosts WHERE ip_address IS NOT NULL',
+        'options': (
+            ('limit', 2, True, 'limit number of api requests per second (0 = unlimited)'),
+        ),
     }
    
     def module_run(self, hosts):
         api_key = self.keys.get('ipinfodb_api')
+        limit = self.options['limit']
+        cnt = 0
         for host in hosts:
             url = (f"http://api.ipinfodb.com/v3/ip-city/?key={api_key}&ip={host}&format=json")
             resp = self.request(url)
@@ -26,7 +32,7 @@ class Module(BaseModule):
             if jsonobj['statusCode'].lower() == 'error':
                 self.error(jsonobj['statusMessage'])
                 continue
-            time.sleep(1.5)
+
             # Used to catch the garbage data and null it out so it does not clog up the database.
             for x in ['cityName', 'regionName', 'countryName', 'latitude', 'longitude']:
                 if jsonobj[x] == '-' or jsonobj[x] == '0':
@@ -37,4 +43,9 @@ class Module(BaseModule):
             latitude = jsonobj['latitude']
             longitude = jsonobj['longitude']
             self.output(f"{host} - {latitude},{longitude} - {', '.join([x for x in [region, country] if x])}")
-            self.query('UPDATE hosts SET region=?, country=?, latitude=?, longitude=? WHERE ip_address=?', (region, country, latitude, longitude, host))
+            self.query('UPDATE hosts SET region=?, country=?, latitude=?, longitude=? WHERE ip_address=?',
+                       (region, country, latitude, longitude, host))
+            cnt += 1
+            if cnt == limit:
+                time.sleep(1.2)
+                cnt = 0
