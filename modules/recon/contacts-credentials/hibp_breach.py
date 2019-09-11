9b76b9b4853cad0ede2e0688ba157ba2921b7e89
Geoff Pamerleau <gpamerleau@gmail.com>
2019-09-10 19:18:11 -0600

    Updated the hibp modules to support v3 of the HIBP API. Resolves #47.

--- modules/recon/contacts-credentials/hibp_breach.py
+++ modules/recon/contacts-credentials/hibp_breach.py
@@ -6,21 +6,23 @@ class Module(BaseModule):
 
     meta = {
         'name': 'Have I been pwned? Breach Search',
-        'author': 'Tim Tomes (@lanmaster53) & Tyler Halfpop (@tylerhalfpop)',
-        'version': '1.0',
+        'author': 'Tim Tomes (@lanmaster53), Tyler Halfpop (@tylerhalfpop) and Geoff Pamerleau (@_geoff_p_)',
+        'version': '1.1',
         'description': 'Leverages the haveibeenpwned.com API to determine if email addresses are associated with breached credentials. Adds compromised email addresses to the \'credentials\' table.',
         'comments': (
             'The API is rate limited to 1 request per 1.5 seconds.',
         ),
+        'required_keys': ['hibp_api'],
         'query': 'SELECT DISTINCT email FROM contacts WHERE email IS NOT NULL',
     }
 
     def module_run(self, accounts):
         # retrieve status
-        base_url = 'https://haveibeenpwned.com/api/v2/{}/{}'
+        headers = {'hibp-api-key': self.keys['hibp_api']}
+        base_url = 'https://haveibeenpwned.com/api/v3/{}/{}?truncateResponse=false'
         endpoint = 'breachedaccount'
         for account in accounts:
-            resp = self.request('GET', base_url.format(endpoint, quote_plus(account)))
+            resp = self.request('GET', base_url.format(endpoint, quote_plus(account)), headers=headers)
             rcode = resp.status_code
             if rcode == 404:
                 self.verbose(f"{account} => Not Found.")
@@ -29,6 +31,6 @@ class Module(BaseModule):
                 continue
             else:
                 for breach in resp.json():
-                    self.alert(f"{account} => Breach found! Seen in the {breach['Title']} breach that occurred on {breach['BreachDate']}.")
+                    self.alert(f"{account} => Breach found! Seen in the {breach['Name']} breach that occurred on {breach['BreachDate']}.")
                 self.insert_credentials(account)
             time.sleep(1.6)
