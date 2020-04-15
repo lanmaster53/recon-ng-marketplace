c9c2966d90914f80cbd0fc6ca240fb6c5cae767e
soier <soier@work>
2020-04-15 19:28:10 +0500

    added updating "contacts" and "locations" tables

--- modules/recon/hosts-netblocks/migrate_hosts.py
+++ modules/recon/hosts-netblocks/migrate_hosts.py
@@ -6,32 +6,44 @@ from ipwhois import IPWhois
 class Module(BaseModule):
 
     meta = {
-        'name': 'Hosts to Netblock Migrator',
+        'name': 'Hosts to Netblocks Migrator',
         'author': 'Andrey Zhukov from USSC',
         'version': '1.0',
-        'description': 'Finding subnets through Whois relying on IP addresses from \'table\' table',
+        'description': 'Finding subnets through Whois relying on IP addresses from \'hosts\' table. Updates \'netblocks\', \'locations\' and \'contacts\' tables.',
         'dependencies': ['ipwhois'],
-        'comments': (
-            '',
-        ),
         'query': 'SELECT DISTINCT ip_address FROM hosts WHERE ip_address IS NOT NULL',
     }
 
     def module_run(self, ip_addresses):
         netblocks = set()
+        addresses = set()
+        emails = set()
         cidr_ranges = []
         for ip in ip_addresses:
             if not ip in cidr_ranges:
                 try:
                     lookup = IPWhois( ip ).lookup_whois()
                     for net in lookup['nets']:
-                        descr = net['name'] or ';'.join( str( net['description'] ).split('\n') )
+                        descr = net['name'] or '; '.join( str( net['description'] ).split('\n') )
                         cidrs = map( lambda x:x.strip(), net['cidr'].split(',') )
                         for cidr in cidrs:
                             netblocks.add( (cidr,descr) )
                             cidr_ranges.extend( self.cidr_to_list(cidr) )
                             self.output( '%s %s (%s)' % (cidr,descr,ip) )
+                        if net['address']:
+                            address = '; '.join( net['address'].split('\n') )
+                            addresses.add(address)
+                            self.output(address)
+                        if net['emails']:
+                            for email in net['emails']:
+                                emails.add(email)
+                                self.output(email) 
                 except Exception as e:
                     self.error( str(e) )
+
         for netblock,descr in netblocks:
-            self.insert_netblocks( netblock=netblock, notes=descr )
+            self.insert_netblocks(netblock=netblock, notes=descr)
+        for address in addresses:
+            self.insert_locations(street_address=address)
+        for email in emails:
+            self.insert_contacts(email=email)
