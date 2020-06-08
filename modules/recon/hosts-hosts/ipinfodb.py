a161dc7b5c4bafd64307827e6cd1cc3e274a9875
jmarr73 <jmarr73@gmail.com>
2020-06-08 13:36:35 -0400

    Updated the ipinfodb module because the delay was too short for resource's rate limit.

--- modules/recon/hosts-hosts/ipinfodb.py
+++ modules/recon/hosts-hosts/ipinfodb.py
@@ -7,7 +7,7 @@ class Module(BaseModule):
     meta = {
         'name': 'IPInfoDB GeoIP',
         'author': 'Tim Tomes (@lanmaster53)',
-        'version': '1.0',
+        'version': '1.1',
         'description': 'Leverages the ipinfodb.com API to geolocate a host by IP address. Updates the \'hosts\' table '
                        'with the results.',
         'required_keys': ['ipinfodb_api'],
@@ -47,4 +47,4 @@ class Module(BaseModule):
             self.query('UPDATE hosts SET region=?, country=?, latitude=?, longitude=? WHERE ip_address=?',
                        (region, country, latitude, longitude, host))
             if self.options['limit']:
-                time.sleep(0.6)
+                time.sleep(0.7)
