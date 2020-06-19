2dc52c4142ee33013ffaf3a20cdc752b8e3c354b
Ryan Hays <hays.ryan@gmail.com>
2020-06-19 00:01:19 -0400

    Added an error check to the binaryedge module to prevent draining the API on error.

--- modules/recon/domains-hosts/binaryedge.py
+++ modules/recon/domains-hosts/binaryedge.py
@@ -5,7 +5,7 @@ class Module(BaseModule):
     meta = {
         'name': 'BinaryEdge.io DNS lookup',
         'author': 'Ryan Hays',
-        'version': '1.1',
+        'version': '1.2',
         'description': 'Uses the BinaryEdge API to discover subdomains.',
         'required_keys': ['binaryedge_api'],
         'query': 'SELECT DISTINCT domain FROM domains WHERE domain IS NOT NULL',
@@ -19,7 +19,8 @@ class Module(BaseModule):
             domain_count = 0
             total_ans = 1
             while domain_count < total_ans:
-                resp = self.request('GET', f"https://api.binaryedge.io/v2/query/domains/dns/{domain}?page={page_num}", headers={'X-Key': key})
+                resp = self.request('GET', f"https://api.binaryedge.io/v2/query/domains/dns/{domain}?page={page_num}",
+                                    headers={'X-Key': key})
                 if resp.status_code == 200:
                     total_ans = resp.json().get('total')
 
@@ -30,3 +31,5 @@ class Module(BaseModule):
                         else:
                             self.insert_hosts(host=subdomain['domain'])
                     page_num += 1
+                elif resp.json().get('status') == 400:
+                    break
