ad09e450f7a48ab8a32af99d506d5be33e10ab4a
Ryan Hays <hays.ryan@gmail.com>
2020-06-22 10:40:56 -0400

    Fixed a bug and updated the shodan_ip module to use the Shodan API library. Resolves #145.

--- modules/recon/hosts-ports/shodan_ip.py
+++ modules/recon/hosts-ports/shodan_ip.py
@@ -1,32 +1,36 @@
 from recon.core.module import BaseModule
-from recon.mixins.search import ShodanAPIMixin
-import re
+import shodan
 
 
-class Module(BaseModule, ShodanAPIMixin):
+class Module(BaseModule):
 
     meta = {
         'name': 'Shodan IP Enumerator',
         'author': 'Tim Tomes (@lanmaster53) and Matt Puckett (@t3lc0)',
-        'version': '1.0',
-        'description': 'Harvests port information from the Shodan API by using the \'ip\' search operator. Updates the \'ports\' table with the results.',
+        'version': '1.1',
+        'description': 'Harvests port information from the Shodan API by using the \'ip\' search operator. Updates the '
+                       '\'ports\' table with the results.',
         'required_keys': ['shodan_api'],
         'query': 'SELECT DISTINCT ip_address FROM hosts WHERE ip_address IS NOT NULL',
         'options': (
             ('limit', 1, True, 'limit number of api requests per input source (0 = unlimited)'),
         ),
+        'dependencies': ['shodan']
     }
 
     def module_run(self, ipaddrs):
         limit = self.options['limit']
+        api = shodan.Shodan(self.keys.get('shodan_api'))
         for ipaddr in ipaddrs:
             self.heading(ipaddr, level=0)
-            query = f"ip:{ipaddr}"
-            results = self.search_shodan_api(query, limit)
-            for host in results:
-                address = host['ip_str']
-                port = host['port']
-                if not host['hostnames']:
-                    host['hostnames'] = [None]
-                for hostname in host['hostnames']:
-                    self.insert_ports(ip_address=address, port=port, host=hostname)
+            try:
+                ipinfo = api.host(ipaddr)
+                for port in ipinfo['data']:
+                    try:
+                        for hostname in port['hostnames']:
+                            self.insert_ports(host=hostname, ip_address=ipaddr, port=port['port'],
+                                              protocol=port['transport'])
+                    except KeyError:
+                        self.insert_ports(ip_address=ipaddr, port=port['port'], protocol=port['transport'])
+            except shodan.exception.APIError:
+                pass
