c1093c0222e39f8164e93c8864ad27c24037906b
lanmaster53 <tjt1980@gmail.com>
2020-06-22 10:17:17 -0400

    Update shodan_ip.py

--- modules/recon/hosts-ports/shodan_ip.py
+++ modules/recon/hosts-ports/shodan_ip.py
@@ -7,7 +7,7 @@ class Module(BaseModule):
     meta = {
         'name': 'Shodan IP Enumerator',
         'author': 'Tim Tomes (@lanmaster53) and Matt Puckett (@t3lc0)',
-        'version': '1.2',
+        'version': '1.1',
         'description': 'Harvests port information from the Shodan API by using the \'ip\' search operator. Updates the '
                        '\'ports\' table with the results.',
         'required_keys': ['shodan_api'],
