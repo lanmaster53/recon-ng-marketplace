d26512b78f6a0943c5da00361d25a872cbfffc5b
Ryan Hays <hays.ryan@gmail.com>
2020-07-01 21:22:36 -0400

    Updated the shodan Pushpin module to use the Shodan API library.

--- modules/recon/locations-pushpins/shodan.py
+++ modules/recon/locations-pushpins/shodan.py
@@ -1,45 +1,76 @@
 from recon.core.module import BaseModule
-from recon.mixins.search import ShodanAPIMixin
+import shodan
+import time
 from datetime import datetime
 
 
-class Module(BaseModule, ShodanAPIMixin):
+def prep_host(host_data, hostname):
+    os = host_data['os']
+    hostname = hostname
+    host_port = f"{host_data['ip_str']}:{host_data['port']}"
+    source = 'Shodan'
+    screen_name = host_port
+    profile_name = host_port
+    profile_url = f"http://{host_port}"
+    media_url = f"https://www.shodan.io/host/{host_data['ip_str']}"
+    thumb_url = 'https://gravatar.com/avatar/ffc4048d63729d4932fd3cc45139174f?s=300'
+    message = (
+        f"Hostname: {hostname} | City: {host_data['location']['city']} | State: {host_data['location']['region_code']} "
+        f"| Country: {host_data['location']['country_name']} | OS: {os}")
+    latitude = host_data['location']['latitude']
+    longitude = host_data['location']['longitude']
+    time = datetime.strptime(host_data['timestamp'], '%Y-%m-%dT%H:%M:%S.%f')
+    return source, screen_name, profile_name, profile_url, media_url, thumb_url, message, latitude, longitude, time
+
+
+class Module(BaseModule):
 
     meta = {
         'name': 'Shodan Geolocation Search',
-        'author': 'Tim Tomes (@lanmaster53)',
-        'version': '1.0',
+        'author': 'Tim Tomes (@lanmaster53) & Ryan Hays (@_ryanhays)',
+        'version': '1.1',
         'description': 'Searches Shodan for media in the specified proximity to a location.',
         'required_keys': ['shodan_api'],
         'comments': (
-            'Shodan \'geo\' searches can take a long time to complete. If receiving timeout errors, increase the global TIMEOUT option.',
+            'Shodan \'geo\' searches can take a long time to complete. If receiving timeout errors, increase the global'
+            ' TIMEOUT option.',
         ),
-        'query': 'SELECT DISTINCT latitude || \',\' || longitude FROM locations WHERE latitude IS NOT NULL AND longitude IS NOT NULL',
+        'query': 'SELECT DISTINCT latitude || \',\' || longitude FROM locations WHERE latitude IS NOT NULL AND '
+                 'longitude IS NOT NULL',
         'options': (
             ('radius', 1, True, 'radius in kilometers'),
             ('limit', 1, True, 'limit number of api requests per input source (0 = unlimited)'),
         ),
+        'dependencies': ['shodan']
     }
 
     def module_run(self, points):
         limit = self.options['limit']
         rad = self.options['radius']
+        api = shodan.Shodan(self.keys.get('shodan_api'))
+
         for point in points:
             self.heading(point, level=0)
             query = f"geo:{point},{rad}"
-            results = self.search_shodan_api(query, limit)
-            for host in results:
-                os = host['os'] if 'os' in host else ''
-                hostname = host['hostnames'][0] if len(host['hostnames']) > 0 else 'None'
-                protocol = f"{host['ip_str']}:{host['port']}"
-                source = 'Shodan'
-                screen_name = protocol
-                profile_name = protocol
-                profile_url = f"http://{protocol}"
-                media_url = f"https://www.shodan.io/host/{host['ip_str']}"
-                thumb_url = 'https://gravatar.com/avatar/ffc4048d63729d4932fd3cc45139174f?s=300'
-                message = (f"Hostname: {hostname} | City: {host['location']['city']}, {host['location']['country_name']} | OS: {os}")
-                latitude = host['location']['latitude']
-                longitude = host['location']['longitude']
-                time = datetime.strptime(host['timestamp'], '%Y-%m-%dT%H:%M:%S.%f')
-                self.insert_pushpins(source, screen_name, profile_name, profile_url, media_url, thumb_url, message, latitude, longitude, time)
+
+            try:
+                page = 1
+                rec_count = 0
+                total_results = 1
+                while rec_count < total_results:
+                    results = api.search(query, page=page)
+                    total_results = results['total']
+
+                    for host in results['matches']:
+                        rec_count += 1
+                        if len(host['hostnames']) > 0:
+                            for hostname in host['hostnames']:
+                                self.insert_pushpins(*prep_host(host, hostname))
+                        else:
+                            self.insert_pushpins(*prep_host(host, 'None'))
+
+                    page += 1
+                    time.sleep(limit)
+
+            except shodan.exception.APIError:
+                pass
