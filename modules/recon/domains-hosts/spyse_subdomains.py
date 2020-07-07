dc2375039e766d6a8348e3c661e1a54d23b0f3ef
Ryan Hays <hays.ryan@gmail.com>
2020-07-07 18:36:29 -0400

    NEW MOD Spyse Subdomain

---
+++ modules/recon/domains-hosts/spyse_subdomains.py
@@ -0,0 +1,42 @@
+from recon.core.module import BaseModule
+
+
+class Module(BaseModule):
+
+    meta = {
+        'name': 'Spyse Subdomain lookup',
+        'author': 'Ryan Hays',
+        'version': '1.0',
+        'description': 'Uses the Spyse API to discover subdomains.',
+        'required_keys': ['spyse_api'],
+        'options': (
+            ('limit', 100, True, 'Limit the number of results returned. Max is 100.'),
+        ),
+        'query': 'SELECT DISTINCT domain FROM domains WHERE domain IS NOT NULL'
+    }
+
+    def module_run(self, domains):
+        key = self.keys.get('spyse_api')
+        for domain in domains:
+            self.heading(domain, level=0)
+            offset = 0
+            total_ans = 1
+            while offset < total_ans:
+                resp = self.request('GET', f"https://api.spyse.com/v3/data/domain/subdomain?offset={offset}&"
+                                           f"limit={self.options['limit']}&domain={domain}",
+                                    headers={"accept": "application/json",
+                                             "Authorization": f"Bearer {key}"})
+
+                if resp.status_code == 200:
+                    data = resp.json()
+                    total_ans =data['data']['max_view_count']
+
+                    for subdomain in data['data']['items']:
+                        offset += 1
+
+                        if subdomain['dns_records']['A'] is not None:
+                            self.insert_hosts(host=subdomain['name'], ip_address=subdomain['dns_records']['A'][0]['ip'])
+                        else:
+                            self.insert_hosts(host=subdomain['name'])
+                elif resp.json().get('status') == 400:
+                    break
