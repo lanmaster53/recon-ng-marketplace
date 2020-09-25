544f34b0354f805c704a94e14294370fd2f4cfc2
Ryan Hays <hays.ryan@gmail.com>
2020-09-25 13:48:59 -0400

    Fixed a bug in the domain scylla module.

--- modules/recon/domains-credentials/scylla.py
+++ modules/recon/domains-credentials/scylla.py
@@ -1,3 +1,5 @@
+import time
+
 from recon.core.module import BaseModule
 
 
@@ -6,7 +8,7 @@ class Module(BaseModule):
     meta = {
         'name': 'Scylla Bulk Credential Harvester',
         'author': 'Tim Tomes (@lanmaster53)',
-        'version': '1.2',
+        'version': '1.3',
         'description': 'Harvests credentials from the scylla.sh API using domains as input. Updates the '
                        '\'credentials\' and \'contacts\' tables with the results.',
         'options': (
@@ -19,11 +21,10 @@ class Module(BaseModule):
         base_url = 'https://scylla.sh/search'
         headers = {'Accept': 'application/json'}
         size = self.options['size']
+        start = 1
         for domain in domains:
-            page = 0
-            _from = size*page
             while True:
-                payload = {'q': f"Email:\"@{domain}\"", 'size': size, 'from': _from}
+                payload = {'q': f"Email:\"@{domain}\"", 'size': size, 'start': start}
                 resp = self.request('GET', base_url, params=payload, headers=headers, auth=('sammy', 'BasicPassword!'))
                 if resp.status_code != 200:
                     self.error('Invalid response.')
@@ -32,13 +33,13 @@ class Module(BaseModule):
                 if not creds:
                     break
                 for cred in creds:
-                    leak = cred['_source'].get('Domain')
-                    username = cred['_source'].get('Email')
-                    password = cred['_source'].get('Password')
-                    passhash = cred['_source'].get('PassHash')
+                    leak = cred['fields'].get('domain')
+                    username = cred['fields'].get('email')
+                    password = cred['fields'].get('password')
+                    passhash = cred['fields'].get('passHash')
                     self.insert_credentials(username=username, password=password, _hash=passhash, leak=leak)
-                page+=1
-                _from = size*page
-                if _from >= 10000:
+                start = start + size
+                if start >= 10000:
                     self.alert('Limit reached.')
                     break
+                time.sleep(0.5)
