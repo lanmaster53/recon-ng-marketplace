4d473e3eea709bccbaa86a6c51ff9e1e1b965843
Ryan Hays <hays.ryan@gmail.com>
2020-10-06 20:11:34 -0400

    Updated the nmap import module to accommodate scans that do not have ports in them.

--- modules/import/nmap.py
+++ modules/import/nmap.py
@@ -5,8 +5,8 @@ from xml.etree import ElementTree
 class Module(BaseModule):
     meta = {
         'name': 'Nmap XML Output Importer',
-        'author': 'Ryan Hays',
-        'version': '1.0',
+        'author': 'Ryan Hays (@_ryanhays)',
+        'version': '1.1',
         'description': 'Imports hosts and ports into the respective databases from Nmap XML output.',
         'options': (
             ('filename', None, True, 'path and filename for list input'),
@@ -19,13 +19,20 @@ class Module(BaseModule):
 
         for host in tree.findall('host'):
             for ip in host.findall('address'):
-                if ip.attrib.get('addrtype') == 'ipv4':
-                    ipaddress = ip.attrib.get('addr')
+                ipaddress = ip.attrib.get('addr')
+                try:
+                    for hostname in host.find('hostnames').findall('hostname'):
+                        self.insert_domains(domain=hostname.attrib.get('name'))
+                        self.insert_hosts(host=hostname.attrib.get('name'), ip_address=ipaddress)
+                except AttributeError:
                     self.insert_hosts(ip_address=ipaddress)
 
-            for host_port in host.find('ports').findall('port'):
-                if host_port.find('state').get('state') != 'open':
-                    continue
-                port = host_port.attrib.get('portid')
-                protocol = host_port.attrib.get('protocol')
-                self.insert_ports(ip_address=ipaddress, port=port, protocol=protocol)
+            try:
+                for host_port in host.find('ports').findall('port'):
+                    if host_port.find('state').get('state') != 'open':
+                        continue
+                    port = host_port.attrib.get('portid')
+                    protocol = host_port.attrib.get('protocol')
+                    self.insert_ports(ip_address=ipaddress, port=port, protocol=protocol)
+            except AttributeError:
+                pass
