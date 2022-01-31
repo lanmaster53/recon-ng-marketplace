234a17fa24e3430c07a4a705b962ee5ce433b8b6
Aidan Holland <aidan@censys.io>
2022-01-31 16:36:00 -0500

    chore(modules): Upgrade Censys Modules

--- modules/recon/hosts-hosts/censys_hostname.py
+++ modules/recon/hosts-hosts/censys_hostname.py
@@ -1,43 +1,54 @@
 from recon.core.module import BaseModule
 
-from censys.search import CensysIPv4
+from censys.search import CensysHosts
 from censys.common.exceptions import CensysException
 
 
 class Module(BaseModule):
     meta = {
-        'name': 'Censys hosts by hostname',
-        'author': 'Censys Team',
-        'version': 2.0,
-        'description': 'Retrieves all IPs for a given hostname. '
-                       'Updates the "hosts" and "ports" tables.',
-        'query': 'SELECT DISTINCT host FROM hosts WHERE host IS NOT NULL',
-        'required_keys': ['censysio_id', 'censysio_secret'],
-        'dependencies': ['censys>=2.0.0'],
+        "name": "Censys - Hosts by Hostname",
+        "author": "Censys, Inc. <support@censys.io>",
+        "version": 2.1,
+        "description": (
+            "Retrieves all IPs for a given hostname. This module queries the"
+            " 'name' field and updates the 'hosts' and 'ports' tables with the"
+            " results."
+        ),
+        "query": "SELECT DISTINCT host FROM hosts WHERE host IS NOT NULL",
+        "required_keys": ["censysio_id", "censysio_secret"],
+        "dependencies": ["censys>=2.1.2"],
     }
 
     def module_run(self, hosts):
-        api_id = self.get_key('censysio_id')
-        api_secret = self.get_key('censysio_secret')
-        c = CensysIPv4(api_id, api_secret)
-        IPV4_FIELDS = [
-            'ip', 'protocols', 'location.country',
-            'location.latitude', 'location.longitude'
-        ]
+        api_id = self.get_key("censysio_id")
+        api_secret = self.get_key("censysio_secret")
+        c = CensysHosts(api_id, api_secret)
         for host in hosts:
+            host = host.strip('"')
             self.heading(host, level=0)
             try:
-                payload = c.search('a:{0}'.format(host), IPV4_FIELDS)
+                query = c.search(f"name:{host}", virtual_hosts="ONLY")
             except CensysException:
+                self.print_exception()
                 continue
-            for result in payload:
+            for hit in query():
+                common_kwargs = {
+                    "ip_address": hit["ip"],
+                    "host": hit.get("name"),
+                }
+                location = hit.get("location", {})
+                coords = location.get("coordinates", {})
                 self.insert_hosts(
-                    host=host,
-                    ip_address=result['ip'],
-                    country=result.get('location.country', ''),
-                    latitude=result.get('location.latitude', ''),
-                    longitude=result.get('location.longitude', ''))
-                for protocol in result['protocols']:
-                    port, service = protocol.split('/')
+                    region=location.get("continent"),
+                    country=location.get("country"),
+                    latitude=coords.get("latitude"),
+                    longitude=coords.get("longitude"),
+                    **common_kwargs,
+                )
+                for service in hit.get("services", []):
                     self.insert_ports(
-                        ip_address=result['ip'], port=port, protocol=service)
+                        port=service["port"],
+                        protocol=service["transport_protocol"],
+                        notes=service["service_name"],
+                        **common_kwargs,
+                    )
--- modules/recon/netblocks-hosts/censys_netblock.py
+++ modules/recon/netblocks-hosts/censys_netblock.py
@@ -1,44 +1,80 @@
 from recon.core.module import BaseModule
 
-from censys.search import CensysIPv4
+from censys.search import CensysHosts
 from censys.common.exceptions import CensysException
 
 
 class Module(BaseModule):
     meta = {
-        'name': 'Censys hosts by netblock',
-        'author': 'Censys Team',
-        'version': 2.0,
-        'description': 'Retrieves hosts and ports for a neyblock. '
-                       'Updates the \'hosts\' and the \'ports\' tables with the results.',
-        'query': 'SELECT DISTINCT netblock FROM netblocks WHERE netblock IS NOT NULL',
-        'required_keys': ['censysio_id', 'censysio_secret'],
-        'dependencies': ['censys>=2.0.0'],
+        "name": "Censys - Hosts by Netblock",
+        "author": "Censys, Inc. <support@censys.io>",
+        "version": 2.1,
+        "description": (
+            "Retrieves hosts and ports for a neyblock. "
+            "Updates the 'hosts' and the 'ports' tables with the results."
+        ),
+        "query": (
+            "SELECT DISTINCT netblock FROM netblocks WHERE netblock IS NOT"
+            " NULL"
+        ),
+        "required_keys": ["censysio_id", "censysio_secret"],
+        "options": [
+            (
+                "virtual_hosts",
+                "EXCLUDE",
+                False,
+                "Whether to include virtual hosts in the results",
+            ),
+            (
+                "per_page",
+                "100",
+                False,
+                "The number of results to return per page",
+            ),
+            (
+                "pages",
+                "1",
+                False,
+                "The number of pages to retrieve",
+            ),
+        ],
+        "dependencies": ["censys>=2.1.2"],
     }
 
     def module_run(self, netblocks):
-        api_id = self.get_key('censysio_id')
-        api_secret = self.get_key('censysio_secret')
-        c = CensysIPv4(api_id, api_secret)
-        IPV4_FIELDS = [
-            'ip', 'protocols', 'location.country',
-            'location.latitude', 'location.longitude'
-        ]
+        api_id = self.get_key("censysio_id")
+        api_secret = self.get_key("censysio_secret")
+        c = CensysHosts(api_id, api_secret)
         for netblock in netblocks:
             self.heading(netblock, level=0)
             try:
-                payload = c.search('ip:{0}'.format(netblock), IPV4_FIELDS)
-            except CensysException:
-                continue
-            for result in payload:
-                ip_address = result['ip']
-                self.insert_hosts(
-                    ip_address=ip_address,
-                    country=result.get('location.country'),
-                    latitude=result.get('location.latitude'),
-                    longitude=result.get('location.longitude'),
+                query = c.search(
+                    f"ip:{netblock}",
+                    per_page=int(self.options.get("PER_PAGE", "100")),
+                    pages=int(self.options.get("PAGES", "1")),
+                    virtual_hosts=self.options.get("VIRTUAL_HOSTS", "EXCLUDE"),
                 )
-                for protocol in result.get('protocols', []):
-                    port, service = protocol.split('/')
+            except CensysException:
+                self.print_exception()
+                continue
+            for hit in query():
+                common_kwargs = {
+                    "ip_address": hit["ip"],
+                    "host": hit.get("name"),
+                }
+                location = hit.get("location", {})
+                coords = location.get("coordinates", {})
+                self.insert_hosts(
+                    region=location.get("continent"),
+                    country=location.get("country"),
+                    latitude=coords.get("latitude"),
+                    longitude=coords.get("longitude"),
+                    **common_kwargs,
+                )
+                for service in hit.get("services", []):
                     self.insert_ports(
-                        ip_address=ip_address, port=port, protocol=service)
+                        port=service["port"],
+                        protocol=service["transport_protocol"],
+                        notes=service["service_name"],
+                        **common_kwargs,
+                    )
