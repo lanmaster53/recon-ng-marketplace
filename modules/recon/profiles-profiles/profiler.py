804dbc8c5cd30472961da072a194f9d5369ce36d
gbinv <60241050+gbinv@users.noreply.github.com>
2023-02-04 12:27:44 -0700

    Update the profiler module. Resolves #233.

--- modules/recon/profiles-profiles/profiler.py
+++ modules/recon/profiles-profiles/profiler.py
@@ -6,8 +6,8 @@ class Module(BaseModule, ThreadingMixin):
 
     meta = {
         'name': 'OSINT HUMINT Profile Collector',
-        'author': 'Micah Hoffman (@WebBreacher)',
-        'version': '1.0',
+        'author': 'Micah Hoffman (@WebBreacher), Brendan Burke (@gbinv)',
+        'version': '1.1',
         'description': 'Takes each username from the profiles table and searches a variety of web sites for those users. The list of valid sites comes from the parent project at https://github.com/WebBreacher/WhatsMyName',
         'comments': (
             'Note: The global timeout option may need to be increased to support slower sites.',
@@ -18,8 +18,8 @@ class Module(BaseModule, ThreadingMixin):
 
     def module_run(self, usernames):
         # retrieve list of sites
-        url = 'https://raw.githubusercontent.com/WebBreacher/WhatsMyName/master/web_accounts_list.json'
-        self.verbose(f"Retrieving {url}...")
+        url = 'https://raw.githubusercontent.com/WebBreacher/WhatsMyName/main/wmn-data.json'
+        self.verbose(f"Retrieving {'url'}...")
         resp = self.request('GET', url)
         for user in usernames: 
             self.heading(f"Looking up data for: {user}")
@@ -29,10 +29,12 @@ class Module(BaseModule, ThreadingMixin):
         d = dict(site)
         if d['valid'] == True:
             self.verbose(f"Checking: {d['name']}")
-            url = d['check_uri'].replace('{account}', quote_plus(user))
+            url = d['uri_check'].replace('{account}', quote_plus(user))
             resp = self.request('GET', url, allow_redirects=False)
-            if resp.status_code == int(d['account_existence_code']):
-                self.debug(f"Codes matched {resp.status_code} {d['account_existence_code']}")
-                if d['account_existence_string'] in resp.text or d['account_existence_string'] in resp.headers:
-                    self.insert_profiles(username=user, url=url, resource=d['name'], category=d['category'])
+            if resp.status_code == int(d['e_code']):
+                self.debug(f"Codes matched {resp.status_code} {d['e_code']}")
+                if d['e_string'] in resp.text or d['e_string'] in resp.headers:
+                    self.insert_profiles(username=user, url=url, resource=d['name'], category=d['cat'])
                     self.query('DELETE FROM profiles WHERE username = ? and url IS NULL', (user,))
+
+
